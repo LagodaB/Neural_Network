86e5d24ba21c3efcd3857eecd28b690bf1bcb1d7
LagodaB <pers.3.03.93.0.o@gmail.com>
2017-05-20 12:58:07 +0300

    Add class and def

--- Neural.py
+++ Neural.py
@@ -3,5 +3,17 @@ import numpy as np
 
 X = np.array(([3,5], [5,1], [10,2]), dtype=float)
 y = np.array(([75],[82], [93]), dtype=float)
+X = X/np.amax(X,axis=0)
+y = y/100 #max test score is 100
 
 print X
+print y
+
+class Neural_Network(object):
+    def __init__(self):
+        #Define HyperParameters
+        self.inputLayersSize = 2
+        self.outputLayerSize = 1
+        self.hiddenLayerSize = 3
+
+        
