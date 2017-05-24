ada076c561aec384721dbdda5244a149e147dfbf
LagodaB <pers.3.03.93.0.o@gmail.com>
2017-05-24 07:13:58 +0300

    Add sigmoid function

--- Neural.py
+++ Neural.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib
 
 
 X = np.array(([3,5], [5,1], [10,2]), dtype=float)
@@ -6,8 +7,8 @@ y = np.array(([75],[82], [93]), dtype=float)
 X = X/np.amax(X,axis=0)
 y = y/100 #max test score is 100
 
-print X
-print y
+print X.shape
+print y.shape
 
 class Neural_Network(object):
     def __init__(self):
@@ -16,4 +17,9 @@ class Neural_Network(object):
         self.outputLayerSize = 1
         self.hiddenLayerSize = 3
 
-        
+def sigmoid(z):
+        #Apply sigmoid activation function
+    return 1/(1+np.exp(-z))
+testInput = np.arange(-6,6,0.01)
+plot(testInput, sigmoid(testInput), linewidth=2)
+grid(1)
